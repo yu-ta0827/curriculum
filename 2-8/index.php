e5ff5626b83f203b768dc8299907b06e6ab67626
yu-ta0827 <74187289+yu-ta0827@users.noreply.github.com>
2020-11-26 21:16:44 +0900

    foreach課題

---
+++ 2-8/index.php
@@ -0,0 +1,8 @@
+<?php
+$fruits = ["apple" =>"といったらりんご", "orange" => "といったらみかん", "peach" => "といったらもも"];
+
+foreach ($fruits as $key => $value) {
+    echo $key;
+    echo $value;
+    echo '<br>';
+}
