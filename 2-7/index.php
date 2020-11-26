123fd005504aaba55e55f22618ac58ea8f00348d
yu-ta0827 <74187289+yu-ta0827@users.noreply.github.com>
2020-11-26 20:43:56 +0900

    配列、連想配列課題

---
+++ 2-7/index.php
@@ -0,0 +1,10 @@
+<?php 
+$iro = ["red" => "赤", "blue" => "青" , "green" => "緑"];
+var_dump($iro);
+echo '<br>';
+?>
+<?php 
+$iro = ["red" => "赤", "blue" => "青" , "green" => "緑"];
+$iro["yellow"] = "黄色";
+var_dump($iro);
+?>
