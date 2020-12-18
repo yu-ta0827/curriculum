2acf2cfb931c7b5eb9cac537bbe45867b696ea60
yu-ta0827 <74187289+yu-ta0827@users.noreply.github.com>
2020-12-18 23:56:30 +0900

    Update index.php

--- 3-3/bugTest/test2/index.php
+++ 3-3/bugTest/test2/index.php
@@ -14,7 +14,7 @@ $signUpMessage = "";
 // ログインボタンが押された場合
 if (isset($_POST["signUp"])) {
     // 1. ユーザIDの入力チェック
-    if (empty($_POST)) {  // 値が空のとき
+    if (empty($_POST["username"])) {  // 値が空のとき
         $errorMessage = 'ユーザーIDが未入力です。';
     } else if (empty($_POST["password"])) {
         $errorMessage = 'パスワードが未入力です。';
