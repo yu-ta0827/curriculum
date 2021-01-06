f6e4a06089a3170da5351292c20e7053f81c5974
yu-ta0827 <74187289+yu-ta0827@users.noreply.github.com>
2021-01-06 09:18:25 +0900

    4章不明点

---
+++ 4-1/db_connect.php
@@ -0,0 +1,19 @@
+<?php
+
+define('DB_DATABASE','YIGroupBlog');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','root');
+define('PDO_DSN','mysql:host=localhost;charset=utf8;dbname='.DB_DATABASE);
+
+
+
+function db_connect(){
+    try{
+        $pdo = new PDO(PDO_DSN,DB_USERNAME,DB_PASSWORD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }catch(PDOException $e){
+        echo "error".$e->getMessage();
+        die();
+    }
+}
